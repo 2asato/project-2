175aee7bad414acb800b9e03c9379e9b26c0cee3
aaronsato <2asato@gmail.com>
2017-08-06 12:11:09 -0700

    edit and delete brewery

--- controllers/breweries.js
+++ controllers/breweries.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Breweries = require('../models/breweries.js');
 
+// delete route
+router.delete('/:id', (req, res)=>{
+    Breweries.findByIdAndRemove(req.params.id, ()=>{
+        res.redirect('/breweries');
+    });
+});
+
+// edit brewery route
+router.get('/:id/edit', (req, res)=>{
+    Breweries.findById(req.params.id, (err, foundBrewery)=>{
+        res.render('breweries'/edit.ejs, {
+            breweries: foundBrewery
+        });
+    });
+});
+
+// edit put route
+router.put('/:id', (req, res)=>{
+    Breweries.findByIdAndUpdate(req.params.id, req.body, ()=>{
+        res.redirect('/breweries');
+    });
+});
+
 // breweries index page
 router.get('/', (req, res)=>{
     Breweries.find({}, (err, foundBrewery)=>{
