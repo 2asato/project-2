d62f200a6a96e631b81700df11849703abe78152
aaronsato <2asato@gmail.com>
2017-08-06 09:11:33 -0700

    create profile page

--- controllers/crew.js
+++ controllers/crew.js
@@ -4,7 +4,12 @@ const router = express.Router();
 // crew route
 router.get('/', (req, res)=>{
   // res.send('hello');
-  res.render('crew/index.ejs');
+    res.render('crew/index.ejs');
 });
+
+// new crew router
+router.get('/new', (req, res)=>{
+    res.render('crew/new.ejs')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 // home route
 app.get('/', (req, res)=>{
@@ -9,6 +10,12 @@ app.get('/', (req, res)=>{
 // use crew controller
 const crewController = require('./controllers/crew.js');
 app.use('/crew', crewController);
+
+// mongoose connection
+mongoose.connect('mongodb://localhost:27017/brewdex');
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+});
 
 app.listen(3000, ()=>{
     console.log('listening....');
