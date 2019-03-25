3b2d724ee9ae2d4ac2f7f65a98a0ef916e3ea435
aaronsato <2asato@gmail.com>
2019-03-25 10:24:54 -0700

    updated navbar links

--- remodel/app.js
+++ remodel/app.js
@@ -1,3 +1,3 @@
-alert("Please excuse the mess. This site is currently being reworked and may not look or work the way its meant to.")
-
-console.log('hi');
+// alert("Please excuse the mess. This site is currently being reworked and may not look or work the way its meant to.")
+//
+// console.log('hi');
